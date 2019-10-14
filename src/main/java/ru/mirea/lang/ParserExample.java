ef57194113408bc2812afc7c3302f96b05e7b1ed
oleg <sobolev_ov@mail.ru>
2019-10-14 20:28:01 +0300

    Initial commit

---
+++ src/main/java/ru/mirea/lang/ParserExample.java
@@ -0,0 +1,45 @@
+package ru.mirea.lang;
+
+public class ParserExample {
+
+    private final char[] symbols;
+    private int pos = 0;
+
+    public ParserExample(char[] symbols) {
+        this.symbols = symbols;
+    }
+
+    private void error(String message) {
+        throw new RuntimeException(message + " в позиции " + pos);
+    }
+
+    private boolean match(char expected) {
+        if (pos < symbols.length) {
+            char c = symbols[pos];
+            if (c == expected) {
+                pos++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void require(char c) {
+        if (!match(c))
+            error("Ожилается " + c);
+    }
+
+    public void parse() {
+        require('Ч');
+        while (pos < symbols.length) {
+            require('+');
+            require('Ч');
+        }
+    }
+
+    public static void main(String[] args) {
+        String text = "Ч+Ч";
+        ParserExample p = new ParserExample(text.toCharArray());
+        p.parse();
+    }
+}
---
+++ src/main/java/ru/mirea/lang/TokenType.java
@@ -0,0 +1,15 @@
+package ru.mirea.lang;
+
+import java.util.regex.Pattern;
+
+public enum TokenType {
+    NUMBER("[0-9]+"),
+    ADD("\\+"),
+    SPACE("[ \t\r\n]+");
+
+    final Pattern pattern;
+
+    TokenType(String regexp) {
+        pattern = Pattern.compile(regexp);
+    }
+}
